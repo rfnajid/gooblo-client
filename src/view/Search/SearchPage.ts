de2075fe190172c616dde7e82fa9fbde3cfaed65
rida <ridhaaaaazis@gmail.com>
2022-08-22 20:15:07 +0700

    UI SearchPage

--- src/view/Search/SearchPage.ts
+++ src/view/Search/SearchPage.ts
@@ -1,7 +1,17 @@
 import { defineComponent, ref } from "vue"
 import { useRoute } from "vue-router"
+import Loading from "../../components/Loading/index.vue"
+import NotFound from "../../components/NotFound/index.vue";
+import ResultItem from "../../components/ResultItem/index.vue";
+import SearchHeader from "../../components/SearchHeader/index.vue";
 
 export default defineComponent({
+    components: {
+        Loading,
+        SearchHeader,
+        ResultItem,
+        NotFound
+    },
     setup () {
         const route = useRoute();
 
@@ -11,14 +21,23 @@ export default defineComponent({
 
         const searchResult = ref([]);
 
+        const onSubmit = () => {
+            // reset
+            searchResult.value = [];
+            // show loading
+            
+            // hit api
+        }
+
         return {
             searchResult: searchResult,
-            query: query
+            query: query,
+            onSubmit: onSubmit
         }
     },
     mounted() {
         console.log('mounted query', this.query);
 
-        
+
     },
 })
