aeec48d7063ec4725ca1413b12fd55dc262feab5
rida <ridhaaaaazis@gmail.com>
2022-08-22 22:08:42 +0700

    Implement searchservice

--- src/components/SearchHeader/SearchHeader.ts
+++ src/components/SearchHeader/SearchHeader.ts
@@ -1,35 +1,23 @@
 import { defineComponent, ref } from "vue"
-import { useRoute } from "vue-router"
 
 export default defineComponent({
-    setup () {
-        const route = useRoute();
-
-        const query = route.query.q;
-
-        console.log('querynya, ', query);
-
-        const searchResult = ref([]);
-
-        const onSubmit = () => {
-            // reset
-            searchResult.value = [];
-            // show loading
-            
-            // hit api
-
-            
+    props: {
+        query: String,
+        onSubmit: Function
+    },
+    setup (props) {
+        const query = ref(props.query);
+        
+        const onSubmit = async () => {
+            if(props.onSubmit) await props.onSubmit(query.value);
         }
 
         return {
-            searchResult: searchResult,
             query: query,
             onSubmit: onSubmit
         }
     },
     mounted() {
         console.log('mounted query', this.query);
-
-
     },
 })
--- src/view/Search/SearchPage.ts
+++ src/view/Search/SearchPage.ts
@@ -1,9 +1,13 @@
-import { defineComponent, ref } from "vue"
-import { useRoute } from "vue-router"
+import { defineComponent, Ref, ref } from "vue"
+import { useRoute, useRouter } from "vue-router"
 import Loading from "../../components/Loading/index.vue"
 import NotFound from "../../components/NotFound/index.vue";
 import ResultItem from "../../components/ResultItem/index.vue";
 import SearchHeader from "../../components/SearchHeader/index.vue";
+import { SearchResult } from "../../model/search-result.model";
+import { search } from "../../services/search/search.service";
+
+const router = useRouter();
 
 export default defineComponent({
     components: {
@@ -15,29 +19,47 @@ export default defineComponent({
     setup () {
         const route = useRoute();
 
-        const query = route.query.q;
+        let query = ref(route.query.q);
+        let isLoading = ref(true);
 
         console.log('querynya, ', query);
 
-        const searchResult = ref([]);
+        const searchResult: Ref<SearchResult[]> = ref([]);
 
-        const onSubmit = () => {
+        const onSubmit = async (newQuery: string) => {
+
+            console.log('newquery on submit', newQuery);
+
+            query.value = newQuery;
+
+            console.log('on submit, query', query.value);
+
             // reset
             searchResult.value = [];
+            
             // show loading
+            isLoading.value = true;
             
             // hit api
+            searchResult.value = await search(query.value);
+
+            isLoading.value = false;
         }
 
         return {
             searchResult: searchResult,
             query: query,
-            onSubmit: onSubmit
+            onSubmit: onSubmit,
+            isLoading: isLoading
         }
     },
     mounted() {
         console.log('mounted query', this.query);
 
-
+        if(!this.query){
+            router.replace('HomePage');
+        }else{
+            this.onSubmit(this.query.toString());
+        }
     },
 })
