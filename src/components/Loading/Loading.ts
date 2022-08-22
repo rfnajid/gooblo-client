9f7f2d9b0900156866d64e386c07ac325914bf19
rida <ridhaaaaazis@gmail.com>
2022-08-22 20:14:16 +0700

    Add Loading component

---
+++ src/components/Loading/Loading.ts
@@ -0,0 +1,36 @@
+import { defineComponent, ref } from "vue"
+import { useRoute } from "vue-router"
+
+export default defineComponent({
+    name: 'Loading',
+    setup () {
+        const route = useRoute();
+
+        const query = route.query.q;
+
+        console.log('querynya, ', query);
+
+        const searchResult = ref([]);
+
+        const onSubmit = () => {
+            // reset
+            searchResult.value = [];
+            // show loading
+            
+            // hit api
+
+            
+        }
+
+        return {
+            searchResult: searchResult,
+            query: query,
+            onSubmit: onSubmit
+        }
+    },
+    mounted() {
+        console.log('mounted query', this.query);
+
+
+    },
+})
---
+++ src/components/Loading/index.vue
@@ -0,0 +1,3 @@
+<template src="./Loading.html"></template>
+<style src="./Loading.css"></style>   
+<script src="./Loading.ts" lang="ts"></script>
